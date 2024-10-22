3019df00ebb398810b76f327a7767e53a515d11b
Kelvinvida99 <kelvinvida99@gmail.com>
2024-10-21 21:54:03 -0400

    queries added

--- httpcrud/data/Repository.php
+++ httpcrud/data/Repository.php
@@ -4,18 +4,21 @@ namespace app\data;
 
 use app\interfaces\RepositoryInterface;
 
-class Repository implements RepositoryInterface {
+class Repository implements RepositoryInterface
+{
     private string $fileData;
     private array $db;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->fileData = __DIR__ . "/data.json";
         $json = file_get_contents($this->fileData);
         $this->db = json_decode($json, true);
     }
-    
-    public function create($data){
-        if(count($this->db) == 0){
+
+    public function create($data)
+    {
+        if (count($this->db) == 0) {
             $data["id"] = 1;
         } else {
             $lastElement = $this->db[count($this->db) - 1];
@@ -33,31 +36,29 @@ class Repository implements RepositoryInterface {
 
     public function update($data)
     {
-        foreach($this->db as $key => $item){
-            if($item["id"] == $data["id"]){
+        foreach ($this->db as $key => $item) {
+            if ($item["id"] == $data["id"]) {
                 $this->db[$key] = $data;
                 file_put_contents($this->fileData, json_encode($this->db));
-                return;
             }
         }
     }
 
     public function delete(int $id)
     {
-        foreach($this->db as $key => $item){
-            if($item["id"] == $id){
+        foreach ($this->db as $key => $item) {
+            if ($item["id"] == $id) {
                 unset($this->db[$key]);
                 $this->db = array_values($this->db);
                 file_put_contents($this->fileData, json_encode($this->db));
-                return;
             }
         }
     }
 
     public function exists(int $id): bool
     {
-        foreach($this->db as $item){
-            if($item["id"] == $id){
+        foreach ($this->db as $item) {
+            if ($item["id"] == $id) {
                 return true;
             }
         }
--- httpcrud/database/RepositoryDB.php
+++ httpcrud/database/RepositoryDB.php
@@ -1,37 +1,55 @@
 <?php
 
 namespace app\database;
+
 use app\interfaces\RepositoryInterface;
 use app\database\BaseRepository;
 use PDO;
 
-class RepositoryDB extends BaseRepository implements RepositoryInterface {
+class RepositoryDB extends BaseRepository implements RepositoryInterface
+{
     const TABLE = "beer";
 
-   public function create($data){
-    return [];
-   }
+    public function create($data)
+    {
+        $sql = "INSERT INTO " . self::TABLE .
+            " (name, alcohol, idBrand) VALUES (:name, :alcohol, :idBrand)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($data);
+    }
 
-   public function get(): array {
-    $sql = "SELECT * FROM " . self::TABLE;
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute();
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $data;
-   }
+    public function get(): array
+    {
+        $sql = "SELECT * FROM " . self::TABLE;
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
 
-   public function update($data)
-   {
-    return [];
-   }
+    public function update($data)
+    {
+        $sql = "UPDATE " . self::TABLE .
+            " SET name = :name, alcohol = :alcohol, idBrand = :idBrand "
+            . "WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($data);
+    }
 
-  public function delete(int $id)
-  {
-   return [];
-  }
+    public function delete(int $id)
+    {
+        $sql = "DELETE FROM " . self::TABLE .
+            " WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+    }
 
- public function exists(int $id): bool
- {
-  return false;
- }
+    public function exists(int $id): bool
+    {
+        $sql = "SELECT * FROM " . self::TABLE
+            . "WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->rowCount() > 0;
+    }
 }
